054a024d446c26e7676096e389a9f8db09e5f675
SophieMac <sophieod95@gmail.com>
2018-04-26 17:21:01 +0100

    Stylings and more pages

---
+++ ChemestyApp/ChemestyApp/Controllers/MatApplicationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MatApplicationViewController.swift
+//  ChemestyApp
+//
+//  Created by Sophie on 26/04/2018.
+//  Copyright © 2018 Edward Attard Montalto. All rights reserved.
+//
+
+import UIKit
+
+class MatApplicationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
